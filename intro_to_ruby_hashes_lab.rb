488677acdb056909edc564b57d428a986bf7e03c
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-13 22:09:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,8 @@ def monopoly_with_third_tier
   	base_hash = {:railroads => {
   	  :pieces => 4,
   	  :rent_in_dollars => {
-  	    :one_piece_owned => {},
-  	    :two_pieces_owned => {},
+  	    :one_piece_owned => 25,
+  	    :two_pieces_owned => 50,
   	    :three_pieces_owned => {},
   	    :four_pieces_owned => {}
   	  }, 
